953b4a9fd056390e619ed850f93d7dd285773fee
David Scott <dave.scott@citrix.com>
2015-09-15 10:01:20 +0100

    Update to new SMAPIv3
    - Rename `Storage.V` to `Storage.Volume`
    - Rename `Storage.P` to `Storage.Plugin`
    - Add field `uuid` for volumes (default to None since we have no natural uuid)
    - Add field `physical_utilisation` for volumes
    - Add field `required_cluster_stack` for the plugin
    - Add field `keys` for volumes
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- volume/common.ml
+++ volume/common.ml
@@ -329,4 +329,4 @@ let vg_of_uri uri =
   | Some "vg" ->
     let vg = Uri.path uri' in
     if vg <> "" && vg.[0] = '/' then String.sub vg 1 (String.length vg - 1) else vg
-  | _ -> raise (Storage.V.SR_does_not_exist uri)
+  | _ -> raise (Storage.Volume.SR_does_not_exist uri)
